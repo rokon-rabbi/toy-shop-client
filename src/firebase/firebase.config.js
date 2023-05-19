b209f67b0d82811440d38ac856b8f2c6eb6da533
rokon <rokonrabbi21@gmail.com>
2023-05-19 10:29:31 +0600

    firebase added to the project

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBHnyaxZreUQUXQFXFKCc-9UP-DqVcqD3w",
+  authDomain: "toy-marketplace-36f98.firebaseapp.com",
+  projectId: "toy-marketplace-36f98",
+  storageBucket: "toy-marketplace-36f98.appspot.com",
+  messagingSenderId: "12617763822",
+  appId: "1:12617763822:web:dbaf08baf2885cef4a1cff"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
