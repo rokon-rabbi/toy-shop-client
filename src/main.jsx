8a52911c08e2ed1c8893b047201a644cbe38a0a8
rokon <rokonrabbi21@gmail.com>
2023-05-19 08:41:50 +0600

    router setup added

--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+
 import './index.css'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import AddCoffe from './Components/AddCoffe.jsx';
+import { RouterProvider } from 'react-router-dom'
+import router from './routes/Routes/Routes'
 
-// import UpdateCoffee from './components/UpdateCoffee.jsx';
-
-const router = createBrowserRouter([
-  // {
-  //   path: "/",
-  //   element: <App></App>,
-  //   loader: () => fetch('http://localhost:5000/coffee')
-  // },
-  {
-    path: "/coffe", 
-    element: <AddCoffe/>
-  }, 
-  // {
-  //   path: 'updateCoffee/:id', 
-  //   element: <UpdateCoffee></UpdateCoffee>, 
-  //   loader: ({params}) => fetch(`http://localhost:5000/coffee/${params.id}`)
-  // }
-]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
