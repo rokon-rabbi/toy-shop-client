da1ec0ab53f27ab06cc6ae347baf8abdb0cb3c15
rokon <rokonrabbi21@gmail.com>
2023-05-20 10:31:58 +0600

    gallery.jsx added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import Slider from "../Slider/Slider";
+import Gallery from "./gallery/Gallery";
 import Subscribe from "./subscribe/Subscribe";
 
 
@@ -7,7 +8,15 @@ const Home = () => {
     <div>
       {/* slider  */}
      <Slider></Slider>
+       {/* gallery  */}
+       <p className="md:text-6xl bg-zinc-50  text-3xl font-extrabold text-gray-900 text-center mt-14 md:mt-12">
+        Gallery
+      </p>
+      <Gallery></Gallery>
+     {/* subscribe  */}
      <Subscribe></Subscribe>
+   
+
     </div>
   );
 };
---
+++ src/pages/Home/gallery/Gallery.jsx
@@ -0,0 +1,43 @@
+import capture1 from "../../../assets/capture.png";
+import capture2 from "../../../assets/1capture.png";
+import capture3 from "../../../assets/2capture.png";
+import capture4 from "../../../assets/3capture.png";
+const Gallery = () => {
+  return (
+    <section className="p-20">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+        {/* Card 1 */}
+        <div className="bg-white rounded-lg ">
+          <img
+            src={capture1}
+            alt="Card 1"
+            className="w-full h-auto cursor-pointer object-cover rounded-xl"
+          />
+        </div>
+        <div className="bg-white rounded-lg ">
+          <img
+            src={capture2}
+            alt="Card 1"
+            className="w-full h-auto object-cover cursor-pointer rounded-xl"
+          />
+        </div>
+        <div className="bg-white rounded-lg">
+          <img
+            src={capture3}
+            alt="Card 1"
+            className="w-full h-auto object-cover cursor-pointer rounded-xl"
+          />
+        </div>
+        <div className="bg-white rounded-lg ">
+          <img
+            src={capture4}
+            alt="Card 1"
+            className="w-full h-auto object-cover cursor-pointer rounded-xl"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Gallery;
